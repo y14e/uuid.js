7373286b72087d3697b274bbc013a450c759f40b
Yusuke Kamiyamane <y@y14e.com>
2025-01-18 22:43:14 +0900

    Create uuid.ts

---
+++ uuid.ts
@@ -0,0 +1,10 @@
+function getUUID(): string {
+  if (typeof crypto.randomUUID === 'function') {
+    return crypto.randomUUID();
+  }
+  const uuid = URL.createObjectURL(new Blob()).slice(-36);
+  URL.revokeObjectURL(uuid);
+  return uuid;
+}
+
+export { getUUID };
